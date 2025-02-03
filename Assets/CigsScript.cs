8a06eda376871c5adb7d545251bb4f6c59e4cba0
Jacob Seiberg <jakeseiberg@gmail.com>
2025-02-03 16:21:53 +0100

    cigs time bonus

--- Assets/CigsScript.cs
+++ Assets/CigsScript.cs
@@ -5,18 +5,22 @@ public class CigsScript : MonoBehaviour
     private SpriteRenderer cigsRenderer;
     public Sprite[] cigsImages;
 
-    static public int cigsCount = 0;
+    static public int cigsCount;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         cigsRenderer = GetComponent<SpriteRenderer>();
+        cigsCount = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
         cigsRenderer.sprite = cigsImages[cigsCount % cigsImages.Length];
-        
+        if(cigsCount > 7){
+            Timer.currentTime += 30;
+            cigsCount = 0;
+        }
     }
 }
