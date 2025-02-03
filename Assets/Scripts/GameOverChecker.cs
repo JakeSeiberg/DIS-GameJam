c28de15b4c8006a14d2172cadfa9011d4c8ebdf2
Jacob Seiberg <jakeseiberg@gmail.com>
2025-02-03 16:00:59 +0100

    game over fix

--- Assets/Scripts/GameOverChecker.cs
+++ Assets/Scripts/GameOverChecker.cs
@@ -3,16 +3,32 @@ using UnityEngine.SceneManagement;
 
 public class GameOverChecker : MonoBehaviour
 {
+    static public int score;
+    private float tmpTime;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        score = 0;   
+        tmpTime = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        updateScore();
+        checkDead();
+    }
+
+    void updateScore(){
+        if(Time.time - tmpTime >= 1){
+            tmpTime = Time.time;
+            score++;
+        }
+    }
+
+    void checkDead(){
         if(Timer.currentTime <= 0){
+            Timer.currentTime = 60;
             SceneManager.LoadScene(2);
         }
     }
