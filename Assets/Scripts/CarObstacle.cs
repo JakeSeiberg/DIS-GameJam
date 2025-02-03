d0227b7019638b4c0a41acc2971e649dac2cf016
Reid Allenstein <reidallenstein@gmail.com>
2025-02-03 13:27:48 -0800

    Added Stationary object spawner to Main and Reid Scene

---
+++ Assets/Scripts/CarObstacle.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class CarObstacle : MonoBehaviour
+{
+    [Tooltip("How fast does the obstacle move in units per second")]
+	public float speed = 15;
+    [Tooltip("How long does the obstacle life before it is automatically destroyed, in seconds")]
+    public float lifeTime = 10;
+    Vector2 direction = new Vector2();
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        direction = new Vector2(0,-1);
+        direction.Normalize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position + new Vector3(direction.x, direction.y, 0) * speed * Time.deltaTime;
+		lifeTime -= Time.deltaTime;
+		if(lifeTime <= 0){
+			Destroy(gameObject);
+		}
+    }
+}
--- Assets/Scripts/StationaryObstacle.cs
+++ Assets/Scripts/StationaryObstacle.cs
@@ -1,9 +1,7 @@
 using UnityEngine;
 
-public class Obstacle_1 : MonoBehaviour
+public class StationaryObstacle : MonoBehaviour
 {
-    [Tooltip("How fast does the obstacle move in units per second")]
-	public float speed = 15;
     [Tooltip("How long does the obstacle life before it is automatically destroyed, in seconds")]
     public float lifeTime = 10;
     Vector2 direction = new Vector2();
@@ -18,7 +16,7 @@ public class Obstacle_1 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = transform.position + new Vector3(direction.x, direction.y, 0) * speed * Time.deltaTime;
+        transform.position += Vector3.down * RoadSpawner.gameSpeed * Time.deltaTime;
 		lifeTime -= Time.deltaTime;
 		if(lifeTime <= 0){
 			Destroy(gameObject);
