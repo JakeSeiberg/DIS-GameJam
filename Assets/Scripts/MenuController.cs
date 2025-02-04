6737b3bba013a99a5996e3bef4413a8a9ecdc206
Jacob Seiberg <jakeseiberg@gmail.com>
2025-02-04 11:51:59 +0100

    progress on settings menu

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -1,8 +1,14 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
+using UnityEngine.Audio;
 public class MenuController : MonoBehaviour
 {
+    public Slider volumeSlider;
+    public AudioMixer mixer;
+    public void setVolume(){
+        mixer.SetFloat("MasterVolume", volumeSlider.value);
+    }
     
     public void OnPlayButton(){
         SceneManager.LoadScene(1);
